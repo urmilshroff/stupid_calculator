7ea3ee5326c9e49ff5ec23cfeafa70e92e0cd35f
Urmil Shroff <urmilshroff@gmail.com>
2018-01-14 22:45:42 +0530

    2 plus 2 is 4, minus 1 that's 3 quick mathsss

--- app/src/main/java/com/urmilshroff/stupidcalculator/MainActivity.java
+++ app/src/main/java/com/urmilshroff/stupidcalculator/MainActivity.java
@@ -2,12 +2,36 @@ package com.urmilshroff.stupidcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void onButtonClick(View v)
+    {
+        TextView t1;
+        EditText e1;
+        EditText e2;
+        int num1, num2, ans;
+
+        e1 = (EditText) findViewById(R.id.input01);
+        e2 = (EditText) findViewById(R.id.input02);
+
+        t1 = (TextView) findViewById(R.id.output);
+
+        num1 = Integer.parseInt(e1.getText().toString());
+        num2 = Integer.parseInt(e2.getText().toString());
+        ans = num1 + num2;
+        t1.setText(Integer.toString(ans));
     }
 }
