f06c74190f8190077314d194f84d1de31a839582
Urmil Shroff <urmilshroff@gmail.com>
2018-03-30 18:15:42 +0530

    Updated

--- app/src/main/java/com/urmilshroff/stupidcalculator/MainActivity.java
+++ app/src/main/java/com/urmilshroff/stupidcalculator/MainActivity.java
@@ -2,9 +2,9 @@ package com.urmilshroff.stupidcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
@@ -23,16 +23,27 @@ public class MainActivity extends AppCompatActivity
         EditText e1;
         EditText e2;
         int num1,num2,ans;
+        //String checknull1,checknull2;
 
         e1=(EditText)findViewById(R.id.input01);
         e2=(EditText)findViewById(R.id.input02);
 
-        num1=Integer.parseInt(e1.getText().toString());
+        num1=Integer.parseInt(e1.getText().toString()); //first it converts EditText to String and then integer
         num2=Integer.parseInt(e2.getText().toString());
 
-        ans=num1+num2;
+        //checknull1=Integer.toString(num1);
+        //checknull2=Integer.toString(num2);
 
-        Toast.makeText(this, "Answer is "+ans, Toast.LENGTH_SHORT).show();
+        if(num1.matches(""))
+        {
+            Toast.makeText(this, "Empty", Toast.LENGTH_SHORT).show();
+        }
+
+        else
+        {
+            ans=num1+num2;
+            Toast.makeText(this, "Answer is " + ans, Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void onSubButtonClick(View v)
